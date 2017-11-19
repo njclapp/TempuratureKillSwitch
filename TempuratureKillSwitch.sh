afb1f4e681e4b2cff6d416c8c72eb2ba3a377766
njclapp <njclapp@pluto.dsu.edu>
2017-11-19 00:13:19 -0600

    Remove Tests

--- TempuratureKillSwitch.sh
+++ TempuratureKillSwitch.sh
@@ -3,12 +3,11 @@
 while true;
 do
     TEMP=$(sensors -f | grep "Core 0:" | cut -d + -f 2 | cut -d . -f1)
-    echo $TEMP
     if [ "$TEMP" -ge 120 ]; then
         echo "$process has been killed" | mailx -s "SUBJECT" user@gmail.com
         killall $daemon
         exit 0
     else
-        sleep 2
+        sleep 5
     fi
 done
